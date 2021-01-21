d839877f8a1a0220baa927ba36c0885617300e3c
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2021-01-21 00:21:20 +0000

    will add book schema

---
+++ db/models/book.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: false
+    },
+    private: {
+        type: Boolean,
+        default: false,
+        required: false
+    },
+    timestamps: true
+})
+
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
