e8e467145b28cab774ae7047a9336f27aac40cec
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2021-01-27 21:27:02 +0000

    created middleware file anded logout route

--- routers/books.js
+++ routers/books.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const asyncErrorHandler = require("../util/asyncErrorHandler");
 const { books, createBooks } = require("../controllers/books");
+const { isLoggedIn } = require('../middleware');
 
 const router = new express.Router();
 
-router.get('/', asyncErrorHandler(books))
+router.get('/',isLoggedIn, asyncErrorHandler(books))
       .post('/', asyncErrorHandler(createBooks))
 
 module.exports = router;
