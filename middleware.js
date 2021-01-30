b7a75fef16e7ce13f3f53c402f14020504558a3b
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2021-01-30 17:37:44 +0000

    add link to new book form and isAuthor middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,6 @@
+const Book = require('./db/models/book');
+const Books = require('./db/models/book');
+
 module.exports.isLoggedIn = ((req, res, next) => {
     console.log('USER', req.user)
     if (!req.isAuthenticated()) {
@@ -5,4 +8,16 @@ module.exports.isLoggedIn = ((req, res, next) => {
         res.redirect('/login');
     }
     next();
-})
+});
+
+module.exports.isAuthor = async ((req, res, next) => {
+    const { id } = req.params;
+
+    const book = await Book.findById(id).exec();
+
+    if (!book.owner.equals(id)) {
+        req.flash('error', 'you do not have permission to do this :-(');
+        return res.redirect(`/books/${id}`);
+    }
+     next();
+});
