48aade70b0fdb226c00ce8ef8a44a0e7fbe5e581
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2021-02-27 23:50:39 +0000

    add reset email fn

--- email/account.js
+++ email/account.js
@@ -25,3 +25,16 @@ module.exports.cancelAccountEmail = async (email, username) => {
 
   await sgMail.send(msg);
 };
+
+module.exports.resetEmail = async (email, token, host) => {
+  const msg = {
+    to: email,
+    from: 'byronleigh80@gmail.com',
+    subject: 'Book Collection = Forgot Password / Reset',
+    text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.
+    Please click on the following link, or copy and paste it into your browser to complete the process:
+    http://${host}/reset/${token}
+    If you did not request this, please ignore this email and your password will remain unchanged.`.replace(/ {12}/g, ''),
+  };
+  await sgMail.send(msg);
+};
